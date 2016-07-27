b80a55d03ed301554153a817e3fa934a5af119ee
AfroMonkey <moisalejandro@gmail.com>
2016-07-27 13:37:13 -0500

    [IMP] Fields to model Add the next fields to the model res.partner     identification_type     identification_number     expiration_date     elector_key

--- models/partner_credential.py
+++ models/partner_credential.py
@@ -2,4 +2,12 @@ from openerp import api, fields, models
 
 
 class PartnerCredential(models.Model):
-    _name = 'partner.credential'
+    _inherit = 'res.partner'
+
+    identification_type = fields.Selection([
+        ('ife', 'IFE'),
+        ('pasaporte', 'Pasaporte'),
+    ])
+    identification_number = fields.Char()
+    expiration_date = fields.Date()
+    elector_key = fields.Char()
