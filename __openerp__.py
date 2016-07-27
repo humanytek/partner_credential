5e58dfed9e606aca20e01801a1eb66229757aaa1
AfroMonkey <moisalejandro@gmail.com>
2016-07-27 13:33:35 -0500

    [IMP] depends

--- __openerp__.py
+++ __openerp__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'author': 'Humanytek',
     'website': 'http://humanytek.com',
-    'depends': [],  # TODO [IMP]
+    'depends': [],
     'data': [
         'views/partner_credential.xml',
     ]
